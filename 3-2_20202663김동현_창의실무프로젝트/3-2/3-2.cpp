25f740b1d5c0403baa6a46da1b982b55636f1d5d
PoisonSul <xyzet3662@gmail.com>
2023-12-07 20:38:26 +0900

    3-3 WEEK

---
+++ 3-2_20202663김동현_창의실무프로젝트/3-2/3-2.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    char ch1, ch2;
+    int i;
+
+    printf("여러 글자를 입력: ");
+    gets_s(str);
+
+    printf("기존 문자와 새로운 문자: ");
+    scanf("%c %c", &ch1, &ch2);
+
+    for (i = 0; i < strlen(str); i++) {
+        if (str[i] == ch1) {
+            str[i] = ch2;
+        }
+    }
+
+    printf("변환된 결과 ==> %s\n", str);
+
+    return 0;
+}
